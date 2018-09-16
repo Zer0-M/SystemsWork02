47300fcde65be94cdb4699b3d142bc4d2901fa7e
Zer0-M <mjamil90@stuy.edu>
2018-09-16 02:47:32 -0400

    finished work02

--- work02.c
+++ work02.c
@@ -3,10 +3,43 @@
 #include <stdlib.h>
 
 int main(){
-  unsigned int i=  ((int)(pow(2,31))+1);
-  char *ip = &i;
+  //Part 1
+  unsigned int x=  ((int)(pow(10,9))*2.5);
+  //Part 2
+  char *xp = &x;
 
-  printf("unsigned int:%u\n",i);
-  printf("char pointer:%p\n",ip);
-  printf("unsigned int in hex:%x\n",i);
+  printf("unsigned int:%u\n",x);
+  printf("char pointer:%p\n",xp);
+  //Part 3
+  printf("unsigned int in hex:%x\n",x);
+  //Part 4
+  printf("bytes in hex:\n");
+  for(int i=0;i<sizeof(i);i++){
+    printf("%d:%hhx\n",i,*xp);
+    xp++;//Incrementation by byte(char = 1 byte)
+  }
+  x=  ((int)(pow(10,9))*2.5);//Values being reset
+  xp = &x;
+
+  //Part 5
+  //Sub-part 1
+  for(int i=0;i<sizeof(i);i++){
+    *xp+=1;//Incrementation of dereferenced value
+    xp++;
+  }
+  printf("unsigned after incrementation by 1:%u\n",x);
+  printf("unsigned after incrementation by 1 in hex:%x\n",x);
+  //Sub-part 2
+  x=  ((int)(pow(10,9))*2.5);
+  xp = &x;
+  for(int i=0;i<sizeof(i);i++){
+    *xp+=16;
+    xp++;
+  }
+  printf("unsigned after incrementation by 16:%u\n",x);
+  printf("unsigned after incrementation by 16 in hex:%x\n",x);
+
+
+
+
 }
